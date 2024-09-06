03676feb8f3ca33b1d483fe650b4d1e67b7c1706
AdeleDst <adele.desaint@gmail.com>
2024-09-06 15:21:57 +0200

    Description du code

--- Adele_documents_d_urbanisme.R
+++ Adele_documents_d_urbanisme.R
@@ -1,1 +1,7 @@
+# À propos du document ----
+# Date : 6 septembre 2024
+# Autrice : Adèle Desaint 
+# Objectif : Ce code a pour objectif de récupérer les données des SCOT et SUP
+# soit les documents d'urbanisme non accessibles grâce à happign 
 
+# 
