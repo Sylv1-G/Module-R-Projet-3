5ab84b8a0844f19e4e08bfd4d010f7d47e246ffa
CyrilGuillaumant <cyril.guillaumant@orange.fr>
2024-09-09 15:26:15 +0200

    Tentative de fusion des tableaux

--- Cyril_MonumentsHistoriques.R
+++ Cyril_MonumentsHistoriques.R
@@ -1,7 +1,7 @@
 # Installation des packages ----
 
 library(librarian)
-shelf(sf, httr,happign,dplyr,tmap)
+shelf(sf, httr,happign,dplyr,tmap,abind)
 library(tmap);ttm()
 
 # Etude des documents d'urbanisme d'une commune ----
@@ -16,60 +16,115 @@ partition <- doc |>
   filter(grid_title == "NANCY") |> 
   pull(partition)
 
-Forets <- get_apicarto_gpu(partition, ressource = "document", 
-                         categorie = c("A1", "A7", "A8"), 
-                         dTolerance = 0)
+Forets <- rbind(get_apicarto_gpu(partition, ressource = "prescription-surf", 
+                                 categorie = c("A1", "A7", "A8"), dTolerance = 0), 
+                get_apicarto_gpu(partition, ressource = "prescription-lin", 
+                                 categorie = c("A1", "A7", "A8"), dTolerance = 0), 
+                get_apicarto_gpu(partition, ressource = "prescription-pct", 
+                                 categorie = c("A1", "A7", "A8"), dTolerance = 0), 
+                get_apicarto_gpu(partition, ressource = "info-surf", 
+                                 categorie = c("A1", "A7", "A8"), dTolerance = 0), 
+                get_apicarto_gpu(partition, ressource = "info-lin", 
+                                 categorie = c("A1", "A7", "A8"), dTolerance = 0), 
+                get_apicarto_gpu(partition, ressource = "info-pct", 
+                                 categorie = c("A1", "A7", "A8"), dTolerance = 0), 
+                get_apicarto_gpu(partition, ressource = "acte-sup", 
+                                 categorie = c("A1", "A7", "A8"), dTolerance = 0), 
+                get_apicarto_gpu(partition, ressource = "generateur-sup-s", 
+                                 categorie = c("A1", "A7", "A8"), dTolerance = 0), 
+                get_apicarto_gpu(partition, ressource = "generateur-sup-l", 
+                                 categorie = c("A1", "A7", "A8"), dTolerance = 0), 
+                get_apicarto_gpu(partition, ressource = "generateur-sup-p", 
+                                 categorie = c("A1", "A7", "A8"), dTolerance = 0), 
+                get_apicarto_gpu(partition, ressource = "assiette-sup-s", 
+                                 categorie = c("A1", "A7", "A8"), dTolerance = 0), 
+                get_apicarto_gpu(partition, ressource = "assiette-sup-l", 
+                                 categorie = c("A1", "A7", "A8"), dTolerance = 0), 
+                get_apicarto_gpu(partition, ressource = "assiette-sup-p", 
+                                 categorie = c("A1", "A7", "A8"), dTolerance = 0))
 
-Littoral <- get_apicarto_gpu(partition, ressource = "document", 
-                          categorie = c("EL9"), 
-                          dTolerance = 0)
+# Littoral <- get_apicarto_gpu(partition, ressource = "document", 
+#                           categorie = c("EL9"), 
+#                           dTolerance = 0)
+# 
+# Eaux <- get_apicarto_gpu(partition, ressource = "document", 
+#                           categorie = c("A4", "AS1"), 
+#                           dTolerance = 0)
+# 
+# RNPN <- get_apicarto_gpu(partition, ressource = "document", 
+#                           categorie = c("AC3", "EL10"), 
+#                           dTolerance = 0)
+# 
+# ZAP <- get_apicarto_gpu(partition, ressource = "document", 
+#                           categorie = c("A9", "A10"), 
+#                           dTolerance = 0)
+# 
+# MHSPR <- get_apicarto_gpu(partition, ressource = "document", 
+#                           categorie = c("AC1", "AC4", "AC4bis"), 
+#                           dTolerance = 0)
+# 
+# MNS <- get_apicarto_gpu(partition, ressource = "document", 
+#                           categorie = c("AC2"), 
+#                           dTolerance = 0)
+# 
+# Sportif <- get_apicarto_gpu(partition, ressource = "document", 
+#                           categorie = c("JS1"), 
+#                           dTolerance = 0)
 
-Eaux <- get_apicarto_gpu(partition, ressource = "document", 
-                          categorie = c("A4", "AS1"), 
-                          dTolerance = 0)
+# Forets <- st_transform(Forets, 2154)
+# 
+# st_write(Forets, "test.gpkg", layer = "Forets", append = FALSE)
+# 
+# Littoral <- st_transform(Littoral, 2154)
+# 
+# st_write(Littoral, "test.gpkg", layer = "Littoral", append = FALSE)
+# 
+# Eaux <- st_transform(Eaux, 2154)
+# 
+# st_write(Eaux, "test.gpkg", layer = "Eaux", append = FALSE)
+# 
+# RNPN <- st_transform(RNPN, 2154)
+# 
+# st_write(RNPN, "test.gpkg", layer = "RNPN", append = FALSE)
+# 
+# ZAP <- st_transform(ZAP, 2154)
+# 
+# st_write(ZAP, "test.gpkg", layer = "ZAP", append = FALSE)
+# 
+# MHSPR <- st_transform(MHSPR, 2154)
+# 
+# st_write(MHSPR, "test.gpkg", layer = "MHSPR", append = FALSE)
+# 
+# MNS <- st_transform(MNS, 2154)
+# 
+# st_write(MNS, "test.gpkg", layer = "MNS", append = FALSE)
+# 
+# Sportif <- st_transform(Sportif, 2154)
+# 
+# st_write(Sportif, "test.gpkg", layer = "Sportif", append = FALSE)
 
-RNPN <- get_apicarto_gpu(partition, ressource = "document", 
-                          categorie = c("AC3", "EL10"), 
-                          dTolerance = 0)
-
-ZAP <- get_apicarto_gpu(partition, ressource = "document", 
-                          categorie = c("A9", "A10"), 
-                          dTolerance = 0)
-
-MHSPR <- get_apicarto_gpu(partition, ressource = "document", 
-                          categorie = c("AC1", "AC4", "AC4bis"), 
-                          dTolerance = 0)
-
-MNS <- get_apicarto_gpu(partition, ressource = "document", 
-                          categorie = c("AC2"), 
-                          dTolerance = 0)
-
-Sportif <- get_apicarto_gpu(partition, ressource = "document", 
-                          categorie = c("JS1"), 
-                          dTolerance = 0)
-
-tmap_options(check.and.fix = TRUE)
-
-tm_shape(Forets)+
-  tm_polygons(col = "darkgreen")
-
-tm_shape(Littoral)+
-  tm_polygons(col = "beige")
-
-tm_shape(Eaux)+
-  tm_polygons(col = "blue")
-
-tm_shape(RNPN)+
-  tm_polygons(col = "green")
-
-tm_shape(ZAP)+
-  tm_polygons(col = "yellow")
-
-tm_shape(MHSPR)+
-  tm_polygons(col = "brown")
-
-tm_shape(MNS)+
-  tm_polygons(col = "lightgreen")
-
-tm_shape(Sportif)+
-  tm_polygons(col = "grey")
+# tmap_options(check.and.fix = TRUE)
+# 
+# tm_shape(Forets)+
+#   tm_polygons(col = "darkgreen")
+# 
+# tm_shape(Littoral)+
+#   tm_polygons(col = "beige")
+# 
+# tm_shape(Eaux)+
+#   tm_polygons(col = "blue")
+# 
+# tm_shape(RNPN)+
+#   tm_polygons(col = "green")
+# 
+# tm_shape(ZAP)+
+#   tm_polygons(col = "yellow")
+# 
+# tm_shape(MHSPR)+
+#   tm_polygons(col = "brown")
+# 
+# tm_shape(MNS)+
+#   tm_polygons(col = "lightgreen")
+# 
+# tm_shape(Sportif)+
+#   tm_polygons(col = "grey")
