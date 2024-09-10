313daedf6352d001c0dd9480566cfbdd86fd1e4f
CyrilGuillaumant <cyril.guillaumant@orange.fr>
2024-09-10 11:25:37 +0200

    Squelette du code propre

---
+++ CodePropre.R
@@ -0,0 +1,47 @@
+# Projet R - SIG _ Sujet 3
+# Extraire les enjeux patrimoniaux d'une zone donnee
+# Authors : Ninon Delattre (GF 24_25, AgroParisTech)
+#           Adele Desaint (GF 24_25, AgroParisTech)
+#           Sylvain Giraudo (GF 24_25, AgroParisTech)
+#           Cyril Guillaumant (GF 24_25, AgroParisTech)
+#           Louise Rovel (GF 24_25, AgroParisTech)
+# Contacts : ninon.delattre@agroparistech.fr
+#           adele.desaint@agroparistech.fr
+#           sylvain.giraudo@agroparistech.fr
+#           cyril.guillaumant@agroparistech.fr
+#           louise.rovel@agroparistech.fr
+# Last update : septembre 2024
+
+# About this code ----
+
+# Description du script : Un script qui permet d'extraire les enjeux 
+# patrimoniaux d'une zone donnée, en lien avec la gestion forestière
+
+# Derniere mise a jour : XX Septembre 2024
+
+# Auteurs : Ninon Delattre
+#           Adèle Desaint
+#           Sylvain Giraudo
+#           Cyril Guillaumant
+#           Louise Rovel
+
+# Institutions :
+# AgroParisTech, 54000 Nancy, France
+
+# Packages installation ----
+
+library(librarian)
+shelf(dplyr, happign, httr, lwgeom, sf, tmap)
+library(tmap);ttm()
+
+# Extraction of INSEE codes from the area ----
+
+XXX
+
+# Studies of planning documents ----
+
+XXX
+
+# Studies of public utility easements (SUP in french) ---
+
+XXX
