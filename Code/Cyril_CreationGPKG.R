4db8e7f07d553727caa0f51f476b8134b7c54617
CyrilGuillaumant <cyril.guillaumant@orange.fr>
2024-09-11 11:25:27 +0200

    XXX

--- Code/Cyril_CreationGPKG.R
+++ Code/Cyril_CreationGPKG.R
@@ -6,7 +6,7 @@ library(tmap);ttm()
 
 # Exporter en GPKG un data frame ----
 
-gpkg_path <- file.path(XXX)
+gpkg_path <- file.path("C:/Users/cyril/Downloads/FIF/ProjetRSIG/Module-R-Projet-3/Code/test.gpkg")
 
 export_to_gpkg <- function(layer, gpkg_path, layer_name = NULL){
   
@@ -16,12 +16,16 @@ export_to_gpkg <- function(layer, gpkg_path, layer_name = NULL){
   if (is.null(layer_name)){
     layer_name <- deparse(substitute(layer))
   }
-  st_write(layer, gpkg_path, layer = layer_name)
+  st_write(layer, gpkg_path, layer = layer_name, append = TRUE, layer_options = "GEOMETRY=GEOMETRY")
 }
 
 # Exporter en GPKG une lidte de data frames ----
 
-dataframes_list <- list(XXX)
+prescriptions <- get_apicarto_gpu("DU_05023", ressource = c("prescription-surf", 
+                                                         "prescription-lin", 
+                                                         "prescription-pct"))
+
+# dataframes_list <- prescriptions
 
 export_list_to_gpkg <- function(dataframes_list, gpkg_path) {
   
