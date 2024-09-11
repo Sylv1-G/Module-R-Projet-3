77adc12b160a9851ec0f7a2dcfc8d60db88091f1
Ninon <ninon.delattre@agroparistech.fr>
2024-09-11 13:44:05 +0200

    ninon_modif fonction selsct;type.sup

--- Code/Code_Ninon.R
+++ Code/Code_Ninon.R
@@ -11,19 +11,21 @@ type.sup <- function(sup){
 # fonction qui renvoie une liste des types voulus
 select.type.sup <- function(sup){
   info_sup <- type.sup(sup)
-  print(info_sup)
+  print("les types présents sont: ")
+  print (info_sup)
   entree <- readline(prompt = "Veuillez entrer une liste des numéros de ligne des types voulus séparés par des virgules (ex : 1,2,3) : ")
-}
   #  Diviser la chaîne en éléments en utilisant la virgule comme séparateur
-  elements <- strsplit(entrée, split = ",")[[1]] 
+  elements <- strsplit(entree, split = ",")[[1]] 
   # Convertir les éléments en nombre si nécessaire
   numeros <- as.numeric(elements)
-  
+  list_type <- list()
+  for (i in numeros)
+    list_type <- append(list_type, info_sup[numeros[i]])
+  return (list_type)
+}
+
 
 # fonction qui renvoie seulement les types voulus par l utilisateur
-# numeros_lignes_types est une liste contenant les numéros des lignes 
-info_sup <- 
-filter.type.sup <-function(sup, numeros_lignes_types){ 
-  sup <- filter(sup,
-                sup$type %in% )
-  
+#filter.type.sup <-function(sup, list_type){ 
+#sup <- filter(sup,
+#sup$type %in% list_type)
