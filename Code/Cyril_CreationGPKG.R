ef88147dcc2e0500f39e3eeccf894bb48757980c
CyrilGuillaumant <cyril.guillaumant@orange.fr>
2024-09-11 13:56:02 +0200

    Code gpkg avec data frame non fusionné

--- Code/Cyril_CreationGPKG.R
+++ Code/Cyril_CreationGPKG.R
@@ -25,14 +25,23 @@ p1 <- get_apicarto_gpu("DU_05023", ressource = "prescription-surf")
 p2 <- get_apicarto_gpu("DU_05023", ressource = "prescription-lin")
 p3 <- get_apicarto_gpu("DU_05023", ressource = "prescription-pct")
 
+layer_names <- c("p1", "p2", "p3")
+
 dataframes_list <- list(p1, p2, p3)
 
-export_list_to_gpkg <- function(dataframes_list, gpkg_path) {
+export_list_to_gpkg <- function(dataframes_list, layer_names, gpkg_path) {
   
-  # Boucle sur chaque élément de la liste
-  for (df in dataframes_list) {
+  # Vérifier que le nombre de noms de couches correspond au nombre de data frames
+  if (length(dataframes_list) != length(layer_names)) {
+    stop("Le nombre de noms de couches doit correspondre au nombre de data frames.")
+  }
+  
+  # Boucle sur chaque élément de la liste avec les noms de couches
+  for (i in seq_along(dataframes_list)) {
+    df <- dataframes_list[[i]]
+    layer_name <- layer_names[i]
     
-    # Appel de la fonction export_to_gpkg pour chaque data frame
-    export_to_gpkg(layer = df, gpkg_path, layer_name = layer_name)
+    # Appel de la fonction export_to_gpkg pour chaque data frame avec le nom de la couche
+    export_to_gpkg(layer = df, gpkg_path = gpkg_path, layer_name = layer_name)
   }
 }
