8db9d30077ca445cf9a8111ea58b42032d3f5fa0
unknown <sylvain.giraudo@agroparistech.fr>
2024-09-11 16:32:26 +0200

    Mise au propre des fonction de trie pour les libelle

--- Code/Code_Ninon.R
+++ Code/Code_Ninon.R
@@ -1,31 +1,39 @@
-# fonction qui donne les differents types des assiettes ou generateurs des sup
-type.sup <- function(sup){
-  if (!is.null(sup)){
-    info_sup <- unique(sup$type)
+
+# fonction qui donne les differents libelles des document d'urbanisme
+libelle.urba <- function(df){
+  if (!is.null(df)){
+    info_df <- unique(df$libelle)
   }else {
-    info_sup <- c()
+    info_df <- c()
   }
-  return(info_sup)
+  return(info_df)
 }
 
-# fonction qui renvoie une liste des types voulus
-select.type.sup <- function(sup){
-  info_sup <- type.sup(sup)
-  print("les types présents sont: ")
-  print (info_sup)
-  entree <- readline(prompt = "Veuillez entrer une liste des numéros de ligne des types voulus séparés par des virgules (ex : 1,2,3) : ")
+# fonction qui renvoie une liste des libelles voulus
+select.libelle.urba <- function(df){
+  info_df <- libelle.urba(df)
+  cat("\nles libelles présents sont: \n\n")
+  print (info_df)
+  cat("\nVeuillez entrer une liste des numéros de ligne des libelles voulus séparés par des virgules \n(ex : 1,2,3) :")
+  entree <- readline(prompt = "")
   #  Diviser la chaîne en éléments en utilisant la virgule comme séparateur
   elements <- strsplit(entree, split = ",")[[1]] 
   # Convertir les éléments en nombre si nécessaire
   numeros <- as.numeric(elements)
-  list_type <- list()
-  for (i in numeros)
-    list_type <- append(list_type, info_sup[numeros[i]])
-  return (list_type)
+  liste_libelle <- info_df[numeros]
+  print(liste_libelle)
+  return(liste_libelle)
 }
 
 
-# fonction qui renvoie seulement les types voulus par l utilisateur
-#filter.type.sup <-function(sup, list_type){ 
-#sup <- filter(sup,
-#sup$type %in% list_type)
+filtre.libelle.urba <- function(df){
+  liste_libelle <- select.libelle.urba(df)
+  
+  df_filter <- dplyr::filter(df,
+                             libelle %in% liste_libelle)
+  
+  return(df_filter)
+}
+
+
+exemple <- filtre.libelle.urba(prescription)
