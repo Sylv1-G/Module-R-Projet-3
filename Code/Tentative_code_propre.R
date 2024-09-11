3eadc3ce00f2125ea935f024f1057ff433f196aa
unknown <sylvain.giraudo@agroparistech.fr>
2024-09-11 16:31:07 +0200

    Redéfinition des codes et libelles

--- Code/Tentative_code_propre.R
+++ Code/Tentative_code_propre.R
@@ -27,45 +27,116 @@ rm(list=ls())
 
 # Importation des données ----
 
-code_prescription <- c("01","07","18","19","25","31", "34", "35", "43")
-libelle_prescription <- c("Espace boisé classé",
-                          "Patrimoine bâti, paysager ou éléments de paysages à protéger",
-                          "Périmètre comportant des orientations d’aménagement et deprogrammation (OAP)",
-                          "Secteur protégé en raison de la richesse du sol et du sous-sol",
-                          "Eléments de continuité écologique et trame verte et bleue",
-                          "Espaces remarquables du littoral",
-                          "Espaces, paysage et milieux caractéristiques du patrimoine naturel et culturel montagnard à préserver",
-                          "Terres nécessaires au maintien et au développement des activités agricoles, pastorales et forestières à préserver",
-                          "Réalisation d’espaces libres, plantations, aires de jeux et de loisir")
+code_prescription <- c("01", "07", "18", "19", "25", "31", "34", "35",
+                       "43", "46", "99")
+libelle_prescription <- c(
+  "Espace boisé classé",
+  "Patrimoine bâti, paysager ou éléments de paysages à protéger",
+  "Périmètre comportant des orientations d’aménagement et deprogrammation (OAP)",
+  "Secteur protégé en raison de la richesse du sol et du sous-sol",
+  "Eléments de continuité écologique et trame verte et bleue",
+  "Espaces remarquables du littoral",
+  "Espaces, paysage et milieux caractéristiques du patrimoine naturel et culturel montagnard à préserver",
+  "Terres nécessaires au maintien et au développement des activités agricoles, pastorales et forestières à préserver",
+  "Réalisation d’espaces libres, plantations, aires de jeux et de loisir",
+  "Constructibilité espace boisé antérieur au 20ème siècle")
 
-code_prescription <- c("01","07","18", "34", "35", "43")
-libelle_prescription <- c("Espace boisé classé",
-                          "Patrimoine bâti, paysager ou éléments de paysages à protéger",
-                          "Périmètre comportant des orientations d’aménagement et deprogrammation (OAP)",
-                          "Espaces, paysage et milieux caractéristiques du patrimoine naturel et culturel montagnard à préserver",
-                          "Terres nécessaires au maintien et au développement des activités agricoles, pastorales et forestières à préserver",
-                          "Réalisation d’espaces libres, plantations, aires de jeux et de loisir")
+code_prescription_patrimonial <- c("01", "07", "18", "31", "34", "35", "43",
+                                   "46", "99")
+libelle_prescription_patrimonial <- c(
+  "Espace boisé classé",
+  "Patrimoine bâti, paysager ou éléments de paysages à protéger",
+  "Périmètre comportant des orientations d’aménagement et deprogrammation (OAP)",
+  "Espaces remarquables du littoral",
+  "Espaces, paysage et milieux caractéristiques du patrimoine naturel et culturel montagnard à préserver",
+  "Terres nécessaires au maintien et au développement des activités agricoles, pastorales et forestières à préserver",
+  "Réalisation d’espaces libres, plantations, aires de jeux et de loisir",
+  "Constructibilité espace boisé antérieur au 20ème siècle")
+
+
+code_prescription_ecologique <- c("01","18", "25", "34", "43", "99")
+libelle_prescription_ecologique <- c(
+  "Espace boisé classé",
+  "Périmètre comportant des orientations d’aménagement et deprogrammation (OAP)",
+  "Eléments de continuité écologique et trame verte et bleue",
+  "Espaces, paysage et milieux caractéristiques du patrimoine naturel et culturel montagnard à préserver",
+  "Réalisation d’espaces libres, plantations, aires de jeux et de loisir")
+
+
+code_info <- c("03", "08", "16", "21", "22","25", "37", "40")
+libelle_info <- c(
+  "Zone de préemption dans un espace naturel et sensible",
+  "Périmètre forestier : interdiction ou réglementation des plantations (code rural et de la pêche maritime), plantations à réaliser et semis d'essence forestière",
+  "Site archéologique",
+  "Projet de plan de prévention des risques",
+  "Protection des rives des plans d'eau en zone de montagne",
+  "Périmètre de protection des espaces agricoles et naturels périurbain",
+  "Bois ou forêts relevant du régime forestier",
+  "Périmètre d’un bien inscrit au patrimoine mondial ou Zone tampon d’un bien inscrit au patrimoine mondial")
+
+code_info_patrimonial <- c("16", "25", "40")
+libelle_info_patrimonial <- c(
+  "Site archéologique",
+  "Périmètre de protection des espaces agricoles et naturels périurbain",
+  "Périmètre d’un bien inscrit au patrimoine mondial ou Zone tampon d’un bien inscrit au patrimoine mondial")
+
+code_info_ecologique <- c("03", "22")
+libelle_info_ecologique <- c(
+  "Zone de préemption dans un espace naturel et sensible",
+  "Protection des rives des plans d'eau en zone de montagne")
+                  
+
+sup_utiles <- c("a1","a7","a8","el9","a4","as1","ac3","el10","a10",
+                "ac1","ac4","ac2","pm1","el2","pm2","pm4","pm5",
+                "pm6","pm7","pm8","pm9")
+libelle_sup <- c(
+  "Serviture de protection des bois et forêts relevant du régime forestier à Mayotte",
+  "Servitude relative aux forêts dites de protection",
+  "Servitures résultant de la mise en défens des terrains et pâturages en montagnes et dunes du Pas-de-Calais",
+  "Servitudes de passage sur le littoral",
+  "Servitudes de passage dans le lit ou sur les berges d'un cours d'eau",
+  "Servitudes résultant de l'instauration de périmètres de protection autour des captaux d'eaux et des sources minérales naturelles",
+  "Réserves naturelles et périmètres de protection autour des réserves naturelles",
+  "Coeur de parc national",
+  "Zones de protection naturelle, agricole et forestière du plateau de Saclay",
+  "Servitudes relatives aux monuments historiques",
+  "Sites patrimoniaux remarquables, zones de protection et de valorisation du patrimoine architectural, urbain et paysager",
+  "Servitudes relatives aux sites inscrits et classés",
+  "Plans de prévention des risques naturels prévisibles (PPRNP) et plans de prévention de risques miniers (PPRM) et documents valant PPRNP",
+  "Servitude qui concerne la Loire et ses affluents",
+  "Servitudes d'inondation pour la rétention des crues du Rhin",
+  "Servitudes autour des installations classées pour la protection de l’environnement et sur des sites pollués, de stockage de déchets ou d’anciennes carrières",
+  "Servitude relative aux zones de rétention d’eau et aux zones dites 'stratégiques pour la gestion de l’eau'",
+  "Servitudes visant à ne pas aggraver les risques pour la sécurité publique en présence d’un ouvrages hydraulique",
+  "Servitudes autour des installations nucléaires de base",
+  "Servitudes relatives aux ouvrages ou infrastructures permettant de prévenir les inondations ou les submersions",
+  "Servitudes relatives à la création, la continuité,la pérennité et l’entretien des équipements de défense des forêts contre les incendies (DFCI)",
+  "Servitudes relatives aux zones de danger")
+
+sup_patrimonial <- c("a10","ac1","ac4","ac2")
+
+libelle_sup_patrimonial <- c(
+  "Zones de protection naturelle, agricole et forestière du plateau de Saclay",
+   "Servitudes relatives aux monuments historiques",
+   "Sites patrimoniaux remarquables, zones de protection et de valorisation du patrimoine architectural, urbain et paysager",
+   "Servitudes relatives aux sites inscrits et classés")
+
+sup_ecologique <- c("a8","a4","as1","ac3","el10","a10")
+libelle_ecologique <- c(
+  "Servitures résultant de la mise en défens des terrains et pâturages en montagnes et dunes du Pas-de-Calais",
+   "Servitudes de passage dans le lit ou sur les berges d'un cours d'eau",
+   "Servitudes résultant de l'instauration de périmètres de protection autour des captaux d'eaux et des sources minérales naturelles",
+   "Réserves naturelles et périmètres de protection autour des réserves naturelles",
+   "Coeur de parc national",
+   "Zones de protection naturelle, agricole et forestière du plateau de Saclay")
 
 
 
-
-
-code_info <- c("03", "08", "16", "21", "22", "37", "40")
-libelle_info <- c("Zone de préemption dans un espace naturel et sensible",
-                  "Périmètre forestier : interdiction ou réglementation des plantations (code rural et de la pêche maritime), plantations à réaliser et semis d'essence forestière",
-                  "Site archéologique",
-                  "Projet de plan de prévention des risques",
-                  "Protection des rives des plans d'eau en zone de montagne",
-                  "Bois ou forêts relevant du régime forestier",
-                  "Périmètre d’un bien inscrit au patrimoine mondial ou Zone tampon d’un bien inscrit au patrimoine mondial")
-
-sup_utiles = list("a1","a7","a8","el9","a4","as1","ac3","el10","a9","a10",
-                  "ac1","ac4","ac2")
-col_utiles_gen = c("gid","suptype","partition","fichier","nomgen","typegen",
+col_utiles_gen <- c("gid","suptype","partition","fichier","nomgen","typegen",
                    "nomsuplitt","geometry")
-col_utiles_ass = c("gid","suptype","partition","fichier","nomass","typeass",
+col_utiles_ass <- c("gid","suptype","partition","fichier","nomass","typeass",
                    "nomsuplitt","geometry")
-noms_def <- c("gid","suptype","partition","fichier","nom","type",
+noms_def <- c("gid","suptype","partition","fichier","nom","libelle",
               "nomsuplitt","geometry")
 
 # fonction ----
@@ -143,8 +214,9 @@ get.sup.gen <- function(x){
   )
   
   
-  
-  colnames(generateur) <- noms_def
+  if(!is.null(generateur)){
+    colnames(generateur) <- noms_def
+  }
   
   return(generateur)
   
@@ -176,8 +248,9 @@ get.sup.ass <- function(x){
     assiette_sup_p[ ,col_utiles_ass]
   )
   
-  colnames(assiette) <- noms_def
-  
+  if(!is.null(assiette)){
+    colnames(assiette) <- noms_def
+  }
   
   return(assiette)
   
@@ -191,9 +264,9 @@ get.gpu.all <- function(x){
   info <- get.gpu.info(x)
   print("info ok")
   sup_gen <- get.sup.gen(x)
-  print("")
+  print("SUP generateur ok")
   sup_ass <- get.sup.ass(x)
-  print("4")
+  print("SUP assiette ok")
   
   all_gpu <- list(prescription,info,sup_gen,sup_ass)
   
