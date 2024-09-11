c1004b080e33616350c72738c1d93554ebbbf2f6
CyrilGuillaumant <cyril.guillaumant@orange.fr>
2024-09-11 16:07:14 +0200

    Mis à jour patie export en gpkg

--- Code/Cyril_CreationGPKG.R
+++ Code/Cyril_CreationGPKG.R
@@ -1,23 +1,9 @@
 # Installation des packages ----
 
 library(librarian)
-shelf(sf, httr,happign,dplyr,tmap,abind)
-library(tmap);ttm()
-
-# Exporter en GPKG un data frame ----
+shelf(sf)
 
 gpkg_path <- file.path("C:/Users/cyril/Downloads/FIF/ProjetRSIG/Module-R-Projet-3/Code/test.gpkg")
-
-export_to_gpkg <- function(layer, gpkg_path, layer_name = NULL){
-  
-  # Si on ne précise pas de nom de couche, le nom de la variable
-  # layer est utilisé (ex : pour routes, la couche s'appellera
-  # "route")
-  if (is.null(layer_name)){
-    layer_name <- deparse(substitute(layer))
-  }
-  st_write(layer, gpkg_path, layer = layer_name)
-}
 
 # Exporter en GPKG une lidte de data frames ----
 
@@ -36,6 +22,6 @@ export_list_to_gpkg <- function(tes_all_gpu, layer_names, gpkg_path) {
     layer_name <- layer_names[i]
     
     # Appel de la fonction export_to_gpkg pour chaque data frame avec le nom de la couche
-    export_to_gpkg(layer = df, gpkg_path = gpkg_path, layer_name = layer_name)
+    st_write(df, gpkg_path, layer_name)
   }
 }
