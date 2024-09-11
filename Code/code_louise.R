9e9c4c7b053f1f789c6ef348029979c5c3a28ebb
louiserovel <louise.rovel@agroparistech.fr>
2024-09-11 23:46:54 +0200

    Mise à jour de la fonction d'affichage générale

--- Code/code_louise.R
+++ Code/code_louise.R
@@ -98,53 +98,83 @@ resultats <- insee.to.partition("56031")
 
 
 
-# Affichage dans R ----
 
-prescriptions <- get_apicarto_gpu(
-  "DU_05023", 
-  ressource = c("prescription-surf","prescription-lin","prescription-pct")
-)
 
-shp <- get_apicarto_cadastre("05023", type = "commune")
+# Affichage dans r ----
 
-# Afficage des prescriptions (ATTENTION BUG si les dataframe sont vides !!)
-
-affichage_prescription <- function(shp, list_urba){
+affichage <- function(shp, gpu_all, type = "Prescriptions"){
   
+  types <- c("Prescriptions", "Informations", "SUP")
+  
+  if (!type %in% types){
+    stop ("Type must be 'Prescriptions', 'Informations' or 'SUP'")
+  }
+
   tmap_mode("view")
   
-  geometry_type <- st_geometry_type(list_urba[[1]])
+  # Definition de l'affichage
+  x <- 1
+  n <- 1
+  if (type == "Informations"){
+    x <- 2
+  } else if (type == "SUP") {
+    x <- 4
+    n <- 2
+  }
   
-  polygones <- list_urba[[1]][geometry_type == "MULTIPOLYGON", ]
-  lignes <- list_urba[[1]][geometry_type == "MULTILINESTRING", ]
-  points <- list_urba[[1]][geometry_type == "MULTIPOINT", ]
   
+  # Creation de la carte interactive
+  map <- tm_shape(shp) +
+    tm_borders(col = "black", lwd = 2) +
+    tm_view(view.legend.position = c("right", "bottom"))
   
-  tm_shape(polygones,
-             group = "Polygones") +
-    tm_fill(col = "libelle",
-            alpha = 0.9,
-            palette = "Spectral",
-            title = "Prescriptions polygones", 
-            legend.show = TRUE) +  
-    tm_borders() +
-    tm_shape(lignes, group = "Lignes") +
-    tm_lines(col = "libelle", 
-             palette = "Accent", 
-             title.col = "Prescriptions lignes") +
-    tm_shape(points, 
-             group = "Points") +
-    tm_symbols(col = "libelle", 
-               palette = "Paired", 
-               shape = 21, 
-               size = 0.2,
-               title.col = "Prescirptions points")  +
-    tm_shape(shp) +
-    tm_borders(col = "black", lwd = 2) + 
-    tm_view(
-      view.legend.position = c("right", "bottom"),
-    )
+  for (i in 1:n){
+    # Separation des types de geometries
+    geometry_type <- st_geometry_type(gpu_all[[x]])
+    
+    polygones <- st_make_valid(gpu_all[[x]][geometry_type == "MULTIPOLYGON", ])
+    lignes <- st_make_valid(gpu_all[[x]][geometry_type == "MULTILINESTRING", ])
+    points <- st_make_valid(gpu_all[[x]][geometry_type == "MULTIPOINT", ])
+    
+    if (nrow(polygones) > 0) {
+      map <- map +
+        tm_shape(polygones, group = "Polygones") +
+        tm_fill(col = "libelle",
+                alpha = ifelse(x == 4, 0.1, 0.9),
+                palette = "Spectral",
+                title = ifelse(x == 4, 
+                               "Assiette SUP", 
+                               paste(type, "surfaciques")), 
+                legend.show = TRUE) +  
+        tm_borders()
+    }
+    
+    if (nrow(lignes) > 0) {
+      map <- map +
+        tm_shape(lignes, group = "Lignes") +
+        tm_lines(col = "libelle", 
+                 palette = "Accent", 
+                 title.col = ifelse(x == 4, 
+                                    "Assiette SUP", 
+                                    paste(type, "lineaires")),
+                 legend.show = TRUE)
+    }
+    
+    if (nrow(points) > 0) {
+      map <- map +
+        tm_shape(points, group = "points") +
+        tm_symbols(col = "libelle",
+                   palette = "Paired",
+                   shape = 21,
+                   size = 0.2,
+                   title.col = paste(type, "ponctuelles"))
+    }
+    x <- x - 1
+  }
+  
+  map
+
 }
 
 
-affichage_prescription(x,tes_all_gpu)
+affichage(shp, gpu_all_2154, type = "SUP")
