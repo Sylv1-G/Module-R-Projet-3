3fab0cc2b45f05ee320e8719f636eaae1c7fb5fd
Ninon <ninon.delattre@agroparistech.fr>
2024-09-11 10:40:38 +0200

    ajout fonctions unique type

--- Code/Code_Ninon.R
+++ Code/Code_Ninon.R
@@ -1,1 +1,12 @@
-
+# fonction qui donne les differents type
+filter.sup <- function(generateur, assiette){
+  if (!is.null(generateur)){
+    generateur <- filter(generateur,
+                  typeinf %in% code_info)
+    info_generateur <- unique(generateur$typeass)
+  }else {
+    info_generateur <- c()
+  }
+  return(info_generateur)
+    
+  }
