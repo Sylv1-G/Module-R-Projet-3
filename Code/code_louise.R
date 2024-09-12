d8b81259aa2b3a005c77a9e427dc06fbc43ed772
louiserovel <louise.rovel@agroparistech.fr>
2024-09-12 13:10:48 +0200

    ajout de commentaires sur les fonctions get.code.insee et insee.to.documents

--- Code/code_louise.R
+++ Code/code_louise.R
@@ -14,7 +14,7 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 rm(list=ls())
 
 
-# Function to get insee code from a shape ----
+# fonction pour obtenir les codes insee des communes associees a une geometrie ----
 get.code.insee <- function(shp){
   if (inherits(shp, c("sf", "sfc"))) {
     communes <- get_apicarto_gpu(shp,"municipality")
@@ -22,47 +22,51 @@ get.code.insee <- function(shp){
   } else {
     stop("x must be of class sf or sfc.")
   }
-  
   return(code_insee)
 }
 
-# Function to get partitions from insee codes ----
-insee.to.partition <- function(code_insee){
+# Fonction pour décrire quels sont les documents d'urbanisme present dans les communes ----
+insee.to.documents <- function(code_insee){
   
   if(!inherits(code_insee,"character")){
     stop("x must be of class character")
   }
   
-  # Is there rnu 
+  # Y a-t-il un rnu associé à ces codes insee ?
   is_rnu <- get_apicarto_gpu(code_insee, ressource = "municipality")
   
+  # Séparation des municipalités avec et sans rnu
   is_rnu_TRUE <- filter(is_rnu, is_rnu == TRUE)
   is_rnu_FALSE <- filter(is_rnu, is_rnu == FALSE)
   
+  # Affichage des communes présentes avec ou sans rnu
   cat("\nCommunes with rnu are:\n")
   print(is_rnu_TRUE$name)
   
   cat("\nCommunes without rnu are:\n")
   print(is_rnu_FALSE$name)
   
+  # Creation de dataframe conenant les documents d'urbanismes 
   doc_urbanisme <- data.frame(grid_title = character(),
                               partition = character(),
                               du_type = character(),
                               stringsAsFactors = FALSE)
   
+  # Parcours de des comunnes non soumises au rnu
   for (i in 1:nrow(is_rnu_FALSE)) {
     
     row <- is_rnu_FALSE[i,]
-    doc <- get_apicarto_gpu(row, "document", dTolerance = 10)
+    doc <- get_apicarto_gpu(row, "document", dTolerance = 10)  # chargement des documents de la commune
     
     if (is.null(doc)) {
       next
     }
     
     doc_filtered <- doc |> 
-      filter(grid_title == row$name)
+      filter(grid_title == row$name) # extraction des documents spécifiques à la commune (PLU, PSMV, CC)
     
-    # Si aucune correspondance, chercher les lignes avec du_type == "PLUi"
+    # Si aucuns documents associes a la commune, 
+    # extraction du PLUi s'il existe
     if (nrow(doc_filtered) == 0) {
       doc_filtered <- doc |>
         filter(du_type == "PLUi")
@@ -70,17 +74,19 @@ insee.to.partition <- function(code_insee){
     
     # Si toujours pas de résultats, afficher un avertissement
     if (nrow(doc_filtered) == 0) {
-      warning(paste("No partitions found for", row$name))
+      warning(paste("No document found for", row$name))
       next
     }
     
-    # Ajouter les lignes filtrées au data.frame
+    # Ajout des documents filtrees au dataframe
     doc_urbanisme <- rbind(doc_urbanisme, 
                            doc_filtered)
   }
   
+  # suppression des dupplicats (par exemple si plusieurs fois le meme PLUi)
   doc_urbanisme_unique <- doc_urbanisme[!duplicated(doc_urbanisme), ]
   
+  # Affichage des partitions de documents d'urbanisme associes aux communes.
   cat("\nThe following town planning documents exist in the surrounding municipalities:\n")
   print (as.data.frame(doc_urbanisme_unique)[,c("grid_title", "partition", "du_type")])
   
@@ -90,9 +96,9 @@ insee.to.partition <- function(code_insee){
 # TESTS
 res <- mapedit::drawFeatures()
 
-resultats <- insee.to.partition(get.code.insee(res))
+resultats <- insee.to.documents(get.code.insee(res))
 
-resultats <- insee.to.partition("56031")
+resultats <- insee.to.documents("56031")
 
 
 
@@ -113,7 +119,7 @@ affichage <- function(shp, gpu_all, type = "Prescriptions"){
   tmap_mode("view")
   
   # Definition de l'affichage
-  x <- 1
+  x <- 1 
   n <- 1
   if (type == "Informations"){
     x <- 2
@@ -121,7 +127,6 @@ affichage <- function(shp, gpu_all, type = "Prescriptions"){
     x <- 4
     n <- 2
   }
-  
   
   # Creation de la carte interactive
   map <- tm_shape(shp) +
@@ -171,10 +176,21 @@ affichage <- function(shp, gpu_all, type = "Prescriptions"){
     }
     x <- x - 1
   }
-  
-  map
+  print(map)
 
 }
 
+affichage.interactif <- function (shp, gpu_all) {
+  
+  cat("\nLes trois carte vont s'afficher au fur et à mesure.\n")
+  
+  for (type in c("Prescriptions", "Informations", "SUP")){
+    
+    cat(paste("\nAffichage des", type))
+    
+    affichage(shp, gpu_all, type)
+  }
+}
 
-affichage(shp, gpu_all_2154, type = "SUP")
+affichage.interactif(shp, gpu_all_2154)
+affichage(shp, gpu_all_2154, type = "Prescriptions")
